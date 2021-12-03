dc0638545d97137ecfd23ae24f44d609925397ab
Suvankar <suvankarroygoswami@gmail.com>
2021-12-03 22:20:24 +0530

    updates

--- djangocrud/mycrudApp/views.py
+++ djangocrud/mycrudApp/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>Hello My First App</h1>");
+    return render(request,'base.html')
